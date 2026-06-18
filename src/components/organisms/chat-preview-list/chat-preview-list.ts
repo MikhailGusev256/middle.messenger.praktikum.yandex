dbb011cb25b0995b583041e7de760fd6ba288683
Mikhail <hookman007@gmail.com>
2026-06-18 22:32:13 +0300

    стилевые правки

--- src/components/organisms/chat-preview-list/chat-preview-list.ts
+++ src/components/organisms/chat-preview-list/chat-preview-list.ts
@@ -9,7 +9,7 @@ export default class ChatPreviewList extends Block {
         {{{ Link text="Профиль >" data-page="profile" }}}
         <input aria-label="Поиск" id="search-chat" class="chat-preview-list__search" type="search" name="search" placeholder="Поиск" autocomplete="off"/>
     </header>
-    <ul>
+    <ul class="chat-preview-list__content" role="list">
         {{#each chats}}
             <li>
                 {{{ ChatPreview name=name message=message time=time count=count src=src dateTime=dateTime }}}
