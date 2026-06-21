3001198fdfa7c13621113b491bac8bd1a07aa20e
Mikhail <hookman007@gmail.com>
2026-06-21 16:34:13 +0300

    регулярки для логина

--- src/components/molecules/login-form/login-form.ts
+++ src/components/molecules/login-form/login-form.ts
@@ -6,8 +6,24 @@ export default class LoginForm extends Form {
   protected template = `
   <form class="login-card__form">
     <div>
-        {{{ Input id="login" name="login" type="text" label="Логин" }}}
-        {{{ Input id="password" name="password" type="password" label="Пароль" }}}
+        {{{ Input 
+            id="login" 
+            name="login" 
+            type="text" 
+            label="Логин" 
+            validation-regex="^(?=.*[a-zA-Z_-])[a-zA-Z0-9_-]{3,20}$"
+            validation-error-text="Неверный формат логина"
+            aria-label="Логин"
+            autocomplete="true" }}}
+        {{{ Input 
+            id="password" 
+            name="password" 
+            type="password" 
+            label="Пароль" 
+            aria-label="Пароль"
+            validation-regex="^(?=.*[A-Z])(?=.*\\d).{8,40}$"
+            validation-error-text="Неверный формат пароля"
+            autocomplete="true" }}}
     </div>
     {{{ Button text="Авторизоваться" type="submit" }}}
   </form>
