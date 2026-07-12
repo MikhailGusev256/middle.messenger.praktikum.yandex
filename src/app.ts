ef90074d56558046892e54ab35c817c7fde900bd
Mikhail <hookman007@gmail.com>
2026-07-12 22:19:57 +0300

    убрано лишнее в app

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,9 @@
-﻿import './components/atoms';
-import './components/helpers';
-import './components/molecules';
-import './components/organisms';
-import {
-  // ContextMap,
-  type TemplateNames,
-} from './components/templates';
-
-import Router from './router/router';
+﻿import Router from './router/router';
 
 export default class App {
-  static initialPage: TemplateNames = 'profile';
-  // private state: {
-  // };
   private router: Router = Router.initialize('#app');
 
-  constructor() {
-    // this.state = {
-    //   currentPage: 'login',
-    // };
-  }
-
   start() {
-    // const context = ContextMap[this.state.currentPage];
     this.router.start();
-    this.router.go(App.initialPage);
   }
 }
