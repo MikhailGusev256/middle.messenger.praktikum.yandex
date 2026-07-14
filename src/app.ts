80ea41025e1568fce3b27fd64f3cdcd1175b7406
Mikhail <hookman007@gmail.com>
2026-07-14 10:00:59 +0300

    temp

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 ﻿import Router from './router/router';
+import userService from './services/user/user-service.ts';
 
 export default class App {
   private router: Router = Router.initialize('#app');
 
   start() {
-    this.router.start();
+    userService
+      .fetchUser()
+      .catch(() => {})
+      .finally(() => this.router.start());
   }
 }
