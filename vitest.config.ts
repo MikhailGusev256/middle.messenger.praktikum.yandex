41d2841b360ea235723254fd8c1508367da45173
Mikhail <hookman007@gmail.com>
2026-07-20 21:09:09 +0300

    vitest

---
+++ vitest.config.ts
@@ -0,0 +1,9 @@
+﻿import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom', // Или 'jsdom' для браузерного окружения
+    include: ['**/*.{test,spec}.{js,ts}'],
+  },
+});
